e47edf2cfb82174ea82de601b92aba3958b50bfb
Ingrid Nymo Søbstad <101103520+Inso04@users.noreply.github.com>
2025-10-01 18:57:25 +0200

    Oppdaterte stringfinder fra innelvering B-1

--- in3120/stringfinder.py
+++ in3120/stringfinder.py
@@ -55,4 +55,72 @@ class StringFinder:
         In a serious application we'd add more lookup/evaluation features, e.g., support for prefix matching,
         support for leftmost-longest matching (instead of reporting all matches), and more.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+
+        # tokenize and normalize, returns (term, span)
+        terms = self._analyzer.terms(buffer)
+
+        # "bookmark" for potential matches
+        # NOTE TO SELF: type hint is not "necessary" but added readability + import 
+        active_states: List[self.State] = []
+
+        # process tokens one by one
+        # goes through (term, span) -> span = (begin, end)
+        for token, (token_begin, token_end) in terms:
+            # start new potential match at tokens pos
+            new_state = self.State(
+                self._trie, # start at root of dict
+                token_begin,# remember where this potential match started
+                "")         # No char matched yet
+            # append to list
+            active_states.append(new_state)
+
+            # process each character of current token
+            for char in token:
+                surviving_states: List[self.State] = []
+                
+                # goes through active states
+                for state in active_states:
+                    next_node = state.node.child(char)
+
+                    # checks if current trie node ^accepts char
+                    if next_node is not None:
+                        # if true
+                        state.node = next_node  # move to next trie node
+                        state.match += char     # add char to match
+                        surviving_states.append(state)# keep state
+                    # if not true, state dies (no match)
+                # replace old with "surviving" active states
+                active_states = surviving_states
+
+            # after process, check if any states complete match
+            new_surviving_states: List[self.State] = []
+
+            for state in active_states:
+                # checks if state is at final node
+                if state.node.is_final():
+                    # found match
+                    phrase = buffer[state.begin : token_end]
+                    clean_phrase = " ".join(phrase.split()) # normalize whitespace
+                    
+                    # yield match
+                    yield self.Result(
+                        state.match,    # matched dict entry
+                        state.node.get_meta(),  # match's metadata
+                        clean_phrase,   # how it appears in original
+                        state.begin,    # start pos in orgininal buffer
+                        token_end)  # end pos in original buffer
+                    
+                # check if match can potentially continue
+                # if more text exists, next char is a space, and trie can continue with space
+                next_node = state.node.child(" ")
+                if (token_end < len(buffer) and buffer[token_end] == " " and (next_node is not None)):
+                    # can continue to next token
+                    state.node = next_node  # advance
+                    state.match += " "      # adds space to match
+                    new_surviving_states.append(state)  # keep for next iter
+
+            # keep states that can continue
+            if token_end < len(buffer) and buffer[token_end] == " ":
+                active_states = new_surviving_states
+            # if no space, can't continue
+        #raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
